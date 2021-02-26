2dd6c7cff2489af5ee60427b77cc43b2a4f1d78c
BramPol <bram-pol@hotmail.com>
2021-02-26 14:52:04 +0100

    Created chunker

--- Assignment 3/preprocessing.R
+++ Assignment 3/preprocessing.R
@@ -3,8 +3,26 @@ library(DescTools)
 library(pROC)
 library(comprehenr)
 
-
+# setwd("~/Documents/M2/Sem2/Natural_Computing/Assignment3/Repo/Natural-Computing/Assignment 3")
 train <- readLines("syscalls/snd-cert/snd-cert.train")
 labels <- readLines("syscalls/snd-cert/snd-cert.1.labels")
+test <- readLines("syscalls/snd-cert/snd-cert.1.test")
+
+
+
+chunker <- function(line, chunklength){
+  n <- chunklength
+  chunks <- substring(line,                     # Apply substring function
+                seq(1, nchar(line), n),
+                seq(n, nchar(line), n))
+  return(chunks)
+}
+
+chunks <- list()
+
+for (line in train){
+  linechunks <- chunker(line, 3)
+  chunks <- append(chunks, linechunks)
+}
 
 
