044a99d5a7963a84c1d1e4a8d52fabba982c1fee
Kaspar <kaspar.daemen@hotmail.com>
2021-02-26 15:01:49 +0100

    ROC Curve

--- Assignment 3/calculate_auc.R
+++ Assignment 3/calculate_auc.R
@@ -1,7 +1,8 @@
-#install.packages("comprehenr")
+#install.packages("ggplot2")
 library(DescTools)
 library(pROC)
 library(comprehenr)
+library(ggplot2)
 
 get_auc <- function(scores, labels) {
   roc_obj <- roc(labels, scores)
@@ -10,7 +11,9 @@ get_auc <- function(scores, labels) {
     FPR=rev(1 - roc_obj$specificities),
     thresholds=rev(roc_obj$thresholds))
   print(auc(roc_obj))
+  
   return <- roc_df
+  
 }
 
 
@@ -21,7 +24,8 @@ tagalog <- read.table('tagalog.test')[,1]
 labels <- c(to_vec(for(i in 1:length(english))  0),  to_vec(for(i in 1:length(tagalog))  1))
 
 roc_df <- get_auc(scores, labels)
-
+ggplot(roc_df, aes(FPR, TPR)) + 
+  geom_path() + labs(title='ROC Curve')
 
 
 
--- Assignment 3/preprocessing.R
+++ Assignment 3/preprocessing.R
@@ -3,6 +3,21 @@ library(DescTools)
 library(pROC)
 library(comprehenr)
 
+chunker <- function(line, chunklength){
+  n <- chunklength
+  chunks <- substring(line,                     # Apply substring function
+                      seq(1, nchar(line), n),
+                      seq(n, nchar(line), n))
+  return(chunks)
+}
+
+preprocess_data <- function (data, chunk_size) {
+  data <- list()
+  labels <- list()
+  for(i in 1:length(data)){
+    mylist <- append(data, chunker(data[i]))
+  }
+}
 
 train <- readLines("syscalls/snd-cert/snd-cert.train")
 labels <- readLines("syscalls/snd-cert/snd-cert.1.labels")
