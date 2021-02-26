c066e2f18215aeac755ea0c16ffdd1104bbad30b
Kaspar <kaspar.daemen@hotmail.com>
2021-02-26 17:38:13 +0100

    Added a For loop that plots all the test files

--- Assignment 3/calculate_auc.R
+++ Assignment 3/calculate_auc.R
@@ -4,7 +4,7 @@ library(pROC)
 library(comprehenr)
 library(ggplot2)
 
-get_auc <- function(scores, labels) {
+get_auc <- function(scores, labels, r) {
   roc_obj <- roc(labels, scores)
   roc_df <- data.frame(
     TPR=rev(roc_obj$sensitivities), 
@@ -12,21 +12,26 @@ get_auc <- function(scores, labels) {
     thresholds=rev(roc_obj$thresholds))
   print(auc(roc_obj))
   
+  p <- ggplot(roc_df, aes(FPR, TPR)) + 
+    geom_path() + labs(title=sprintf("Roc curve of r = %d, the AUC = %f", r, auc(roc_obj)))
+  print(p)
   return <- roc_df
   
 }
 
-
-scores <- read.table('test_results.txt')[,1]
 english <- read.table('english.test')[,1]
 tagalog <- read.table('tagalog.test')[,1]
 
 labels <- c(to_vec(for(i in 1:length(english))  0),  to_vec(for(i in 1:length(tagalog))  1))
+r_values <- seq(1,9)
 
-roc_df <- get_auc(scores, labels)
-ggplot(roc_df, aes(FPR, TPR)) + 
-  geom_path() + labs(title='ROC Curve')
-
+for (r in r_values) {
+  scores <- read.table(sprintf("Results/test_results_%d.txt", r))[,1]
+  print(sprintf("Results/test_results_%d.txt", r))
+  print(length(scores))
+  roc_df <- get_auc(scores, labels, r)
+  
+}
 
 
 
