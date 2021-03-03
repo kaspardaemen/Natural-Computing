d81050071788c9908ea264e606971d2910da6e04
Kaspar <kaspar.daemen@hotmail.com>
2021-03-03 16:39:06 +0100

    Added a label list for the preprocessing of the test files

--- Assignment 3/preprocessing.R
+++ Assignment 3/preprocessing.R
@@ -2,14 +2,14 @@
 library(DescTools)
 library(pROC)
 library(comprehenr)
+library(zeallot)
 
 # setwd("~/Documents/M2/Sem2/Natural_Computing/Assignment3/Repo/Natural-Computing/Assignment 3")
 train <- readLines("syscalls/snd-cert/snd-cert.train")
-labels <- readLines("syscalls/snd-cert/snd-cert.1.labels")
-test <- readLines("syscalls/snd-cert/snd-cert.1.test")
 
-print(head(labels))
-print(head(train))
+#########################
+#preprocess the train set
+#########################
 
 chunker <- function(line, chunklength){
   n <- chunklength
@@ -20,10 +20,46 @@ chunker <- function(line, chunklength){
 }
 
 chunks <- list()
-for (line in train){
-  linechunks <- chunker(line, 7)
+chunk.labels <- list()
+for (i in (1:length(train))){
+  line <- train[i]
+  c(linechunks, linelabels) <- chunker(line, 7) # used %<-% to unpack a list into two variables
   chunks <- append(chunks, linechunks)
 }
 
 vector_chunks <- unlist(chunks)
 write(vector_chunks, file = "seven_chunks.txt", ncolumns = 1)
+
+
+########################
+#preprocess the test set
+########################
+
+labels <- readLines("syscalls/snd-cert/snd-cert.1.labels")
+test <- readLines("syscalls/snd-cert/snd-cert.1.test")
+
+chunker <- function(line, chunklength, label){
+  n <- chunklength
+  chunks <- substring(line,                     # Apply substring function
+                      seq(1, nchar(line), n),
+                      seq(n, nchar(line), n))
+  labels <- to_vec(for(i in 1:length(chunks))  label)
+  return(list(chunks, labels))
+}
+
+chunks <- list()
+chunk.labels <- list()
+for (i in (1:length(test))){
+  line <- test[i]
+  label <- labels[i]
+  c(linechunks, linelabels) %<-% chunker(line, 7, label ) # used %<-% to unpack a list into two variables
+
+  chunks <- append(chunks, linechunks)
+  chunk.labels <- append(chunk.labels, linelabels)
+}
+
+vector_chunks <- unlist(chunks)
+vector_chunk_labels <- unlist(chunk.labels)
+write(vector_chunks, file = "seven_chunks_test.txt", ncolumns = 1)
+write(vector_chunk_labels, file = "seven_chunks_test_labels.txt", ncolumns = 1)
+
